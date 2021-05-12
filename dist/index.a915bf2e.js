a5324d8d631ee3c2bdf00f2c08401394d4b0bd4d
IcyPenguinoz <72772015+IcyPenguinoz@users.noreply.github.com>
2021-05-13 00:33:40 +1000

    commit6

--- dist/index.a915bf2e.js
+++ dist/index.a915bf2e.js
@@ -445,6 +445,7 @@ id) /*: string*/
 var _componentsNavigation = require('./components/navigation');
 var _parcelHelpers = require("@parcel/transformer-js/lib/esmodule-helpers.js");
 var _componentsNavigationDefault = _parcelHelpers.interopDefault(_componentsNavigation);
+require('./components/tasklist');
 const links = document.querySelectorAll('.top-nav > ul > li > a');
 const pages = document.querySelectorAll('.page-container');
 var nav = new _componentsNavigationDefault.default(links, pages);
@@ -465,7 +466,7 @@ subNav.links.forEach(link => {
   });
 });
 
-},{"./components/navigation":"2K1cj","@parcel/transformer-js/lib/esmodule-helpers.js":"5gA8y"}],"2K1cj":[function(require,module,exports) {
+},{"./components/navigation":"2K1cj","@parcel/transformer-js/lib/esmodule-helpers.js":"5gA8y","./components/tasklist":"Rj9Cl"}],"2K1cj":[function(require,module,exports) {
 var _parcelHelpers = require("@parcel/transformer-js/lib/esmodule-helpers.js");
 _parcelHelpers.defineInteropFlag(exports);
 class Navigation {
@@ -539,6 +540,71 @@ exports.export = function (dest, destName, get) {
     get: get
   });
 };
+},{}],"Rj9Cl":[function(require,module,exports) {
+const form = document.getElementById("taskform")
+const button = document.querySelector("#taskform > button")
+var taskInput = document.getElementById("taskInput");
+var tasklist = document.querySelector("#tasklist > ul");
+
+var dueDateInput = document.getElementById("dueDateInput");
+var completionTimeInput = document.getElementById("completionTimeInput");
+var estimatedTimeInput = document.getElementById("estimatedTimeInput");
+var priorityInput = document.getElementById("priorityInput");
+
+button.addEventListener("click", function(event){
+  event.preventDefault();
+  let task = taskInput.value;
+  let dueDate = dueDateInput.value;
+  let completionTime = completionTimeInput.value;
+  let estimatedTime = estimatedTimeInput.value;
+  let priorityRating = priorityInput.options[priorityInput.selectedIndex].value;
+  addTask(task, dueDate, priorityRating, estimatedTime, completionTime, false);
+  console.log(taskList);
+})
+
+var taskListArray = [];
+
+function addTask(taskDescription, dueDate, priorityRating, estimatedTime, completionTime, completionStatus) {
+  let d = new Date();
+  let dateCreated = d.getFullYear(); 
+  let task = {
+    taskDescription,
+    dueDate,
+    dateCreated,
+    priorityRating,
+    estimatedTime,
+    completionTime,
+    completionStatus
+  };
+  taskListArray.push(task);
+  renderTask(task);
+  
+}
+
+
+function renderTask(task){
+  // Create HTML elements
+  let item = document.createElement("li");
+  item.innerHTML = "<p>" + task.taskDescription + "</p>";
+
+  tasklist.appendChild(item)
+
+  // Extra Task DOM elements 
+  let delButton = document.createElement("button");
+  let delButtonText = document.createTextNode("Delete Task");
+  delButton.appendChild(delButtonText);
+  item.appendChild(delButton);
+
+
+  //Event Listeners for DOM elements
+  delButton.addEventListener("click", function(event){
+    event.preventDefault();
+    item.remove();
+  })
+
+  // Clear the input form 
+  form.reset();
+}
 },{}]},["4de8m","4B4Nd"], "4B4Nd", "parcelRequirec526")
 
 //# sourceMappingURL=index.a915bf2e.js.map
