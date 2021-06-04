48ded0f5e52ec05add5e13e181fd479f1502a5a9
IcyPenguinoz <72772015+IcyPenguinoz@users.noreply.github.com>
2021-06-04 23:03:20 +1000

    commit30 - kanbanBoard buttons

--- dist/index.8426f084.js
+++ dist/index.8426f084.js
@@ -452,14 +452,18 @@ var report = function() {
 };
 $('#generate').click(function(){
     var myVar = $('#inputField').val();
-
     var words = myVar.split(" ");
+    var showWords = myVar.split(",").join("<br />")
     var acronym = words.map(function(word) {
     return word.charAt(0).toUpperCase();
     }).join('');
     $("#result").html(acronym); //acronym is variable which should be shown on head/title of div card
-    $("#acronymBoxContainer").append("<div class = task-list-boxes>" + "<h2>" + acronym + "</h2>" + "</div>"); //this appends new acronyms to div elements 
+    $("#acronymBoxContainer").append("<div class = task-list-boxes>" + "<h2>" + acronym + "</h2>" + "<p>" + showWords + "</p>" + "</div>"); //this appends new acronyms to div elements 
     console.log(acronym);
+    console.log(myVar);
+    console.log(showWords);
+    
+
 
 });
 
